b858a8fa4941450afd264ddc1234961d5e45e265
Angelos Makris <sade-1995@hotmail.com>
2023-06-14 00:07:27 +0300

    google places + autocomplete

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -8,11 +8,4 @@ window.Stimulus   = application
 
 export { application }
 
-var map;
-function initializeMap() {
-  var mapOptions = {
-    zoom: 10,
-    // Additional map options
-  };
-  map = new google.maps.Map(document.getElementById('map'), mapOptions);
-}
+
